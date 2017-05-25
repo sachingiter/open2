6626495f64bad7042c29085fca9d6fc66c6322f7
sachin patidar <sachin.patidar4@gmail.com>
2017-05-25 11:27:40 +0530

    first commit

---
+++ www/js/menu.js
@@ -0,0 +1,16 @@
+﻿angular.module('menu.controllers', [])
+
+.controller('menuCtrl', function ($scope, $state, $rootScope, $ionicModal) {
+   
+    $ionicModal.fromTemplateUrl('views/navmodal.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+
+    });
+
+    $scope.openModal = function () {
+        $scope.modal.show();
+    }
+
+})
